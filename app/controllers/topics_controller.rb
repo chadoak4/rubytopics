035c27e6d7bda888ef9abf45db86fe9b6d78e909
Chad Oakley <Chad.Oakley@tampabay.rr.com>
2015-10-11 14:08:41 -0400

    here we go

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,10 +1,15 @@
 class TopicsController < ApplicationController
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def index
+    @topics = Topic.all.order("created_at DESC")
   end
 
   def new
     @topic = Topic.new
+  end
+
+  def show
   end
 
   def create
@@ -17,10 +22,28 @@ class TopicsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @topic.update(topic_params)
+      redirect_to topic_path(@topic)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  end
+
   private
 
     def topic_params
       params.require(:topic).permit(:title, :description, :focus)
+    end
+
+    def find_book
+      @topic = Topic.find(params[:id])
     end
 
 end
